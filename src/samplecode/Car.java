044543d9d88ef8167c41ff50924410df15a6853b
AndreT5682 <81456353+AndreT5682@users.noreply.github.com>
2021-04-21 16:14:56 -0400

    Initial commit

---
+++ src/samplecode/Car.java
@@ -0,0 +1,21 @@
+package samplecode;
+
+public class Car {
+
+	public static void main(String[] args) {
+		Car myCar = newCar("Move");
+
+		int distance = 1;
+
+		if (distance > 0) ;
+
+		System.out.println("Moving");
+	}
+
+
+
+
+	public static Car newCar(String move) {
+	return null;
+    }
+}
